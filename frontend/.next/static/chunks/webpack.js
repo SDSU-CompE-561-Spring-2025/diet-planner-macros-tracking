6f4d25472af6a020427611ef1f4fcd105058b7a9
SarahAz20 <sazzalddin8758@sdsu.edu>
2025-05-03 19:52:24 -0700

    Add hot update files for webpack with new hash values
    - Created new hot-update JSON files for webpack.
    - Added hot-update JavaScript files with eval-source-map for development.
    - Updated webpack runtime modules to include compatibility for default exports and hash retrieval.

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -129,7 +129,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e41be183d3f24284")
+/******/ 		__webpack_require__.h = () => ("71422c80dc38f37c")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
