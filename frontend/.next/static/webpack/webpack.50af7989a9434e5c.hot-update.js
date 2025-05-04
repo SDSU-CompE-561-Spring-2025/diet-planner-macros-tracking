be40caedd2b0c5208d204d4e8c509ad006af663e
SarahAz20 <sazzalddin8758@sdsu.edu>
2025-05-03 19:28:52 -0700

    Implement code changes to enhance functionality and improve performance across various modules.

--- frontend/.next/static/webpack/webpack.50af7989a9434e5c.hot-update.js
+++ frontend/.next/static/webpack/webpack.50af7989a9434e5c.hot-update.js
@@ -9,21 +9,9 @@
  */
 self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
-/******/ /* webpack/runtime/compat get default export */
-/******/ (() => {
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = (module) => {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			() => (module['default']) :
-/******/ 			() => (module);
-/******/ 		__webpack_require__.d(getter, { a: getter });
-/******/ 		return getter;
-/******/ 	};
-/******/ })();
-/******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ (() => {
-/******/ 	__webpack_require__.h = () => ("51a2f7c12dec9f6b")
+/******/ 	__webpack_require__.h = () => ("0a2b321a8325d7b1")
 /******/ })();
 /******/ 
 /******/ }
