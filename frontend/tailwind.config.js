d61d9ca7f2a62fafe073afa950abea5786f457d2
haileyzema <haileyzema@gmail.com>
2025-05-07 19:48:51 -0700

    Apply global theme styles to layout, footer, and navbar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ['Marcellus', 'serif'],
+        body: ['Mulish', 'sans-serif'],
+      },
+      colors: {
+        forest: '#173505',
+        cream: '#fbf7e4',
+        ivory: '#f5f1cd',
+        amber: '#ffc95c',
+        orange: '#ef9821',
+        brightOrange: '#f46b24',
+        raspberry: '#c92e52',
+        mint: '#96ae6d',
+        olive: '#7c9853',
+        moss: '#425422',
+      },
+    },
   },
   plugins: [],
 };
